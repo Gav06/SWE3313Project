c8ceaf6059f31ecccdd52ac0915094480129db53
Gavin <72846934+Gav06@users.noreply.github.com>
2025-11-20 00:10:27 -0500

    Cleanup & Refactor some backend code, login requests still not working yet but it will be handled soon. Also moved CSS to its own file to be reused everywhere.

--- src/main/java/team/lebron/ApiController.java
+++ src/main/java/team/lebron/ApiController.java
@@ -3,10 +3,7 @@ package team.lebron;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -19,5 +16,19 @@ public class ApiController {
         response.put("hello", "Hello from SpringBoot!");
         response.put("team", "Team LeBron");
         return response;
+    }
+
+    @PostMapping("/login")
+    public Map<String, String> login(@RequestBody LoginRequest logReq) {
+        System.out.println("Attempted login request");
+        final LoginResponse logRes = processLogin(logReq);
+        final HashMap<String, String> response = new HashMap<>();
+        response.put("success", String.valueOf(logRes.isSuccess()));
+        response.put("message", logRes.getMessage());
+        return response;
+    }
+
+    private LoginResponse processLogin(LoginRequest logReq) {
+        return new LoginResponse(true, "Login success");
     }
 }
---
+++ src/main/java/team/lebron/LoginResponse.java
@@ -0,0 +1,14 @@
+package team.lebron;
+
+public class LoginResponse {
+    private boolean success;
+    private String message;
+
+    public LoginResponse(boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+
+    public boolean isSuccess() { return success; }
+    public String getMessage() { return message; }
+}
